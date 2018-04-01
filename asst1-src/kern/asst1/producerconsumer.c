116fd251dfa8e45635aa2b537d794ccb50358dc4
z5115087 <z5115087@cse.unsw.edu.au>
2018-04-01 17:31:19 +1000

    q3

--- asst1-src/kern/asst1/producerconsumer.c
+++ asst1-src/kern/asst1/producerconsumer.c
@@ -1,51 +1,77 @@
-/* This file will contain your solution. Modify it as you wish. */
-#include <types.h>
-#include "producerconsumer_driver.h"
-
-/* Declare any variables you need here to keep track of and
-   synchronise your bounded. A sample declaration of a buffer is shown
-   below. You can change this if you choose another implementation. */
-
-static struct pc_data buffer[BUFFER_SIZE];
+	/* This file will contain your solution. Modify it as you wish. */
+	#include <types.h>
+	#include "producerconsumer_driver.h"
+	#include <synch.h>  // for P(), V(), sem_* 
 
 
-/* consumer_receive() is called by a consumer to request more data. It
-   should block on a sync primitive if no data is available in your
-   buffer. */
+	/* Declare any variables you need here to keep track of and
+	   synchronise your bounded. A sample declaration of a buffer is shown
+	   below. You can change this if you choose another implementation. */
 
-struct pc_data consumer_receive(void)
-{
-        struct pc_data thedata;
+	static struct {
+		struct pc_data elements[BUFFER_SIZE];
+		int first; /* buf[first%BUFF_SIZE] is the first empty slot  */
+		int last; //buf[last%BUFF_SIZE] is the first full slot  
+	} buffer;
 
-        (void) buffer; /* remove this line when you start */
+	//typedef int semaphore;
+	static struct semaphore *mutex, *empty, *full;
 
-        /* FIXME: this data should come from your buffer, obviously... */
-        thedata.item1 = 1;
-        thedata.item2 = 2;
+	/* consumer_receive() is called by a consumer to request more data. It
+	   should block on a sync primitive if no data is available in your
+	   buffer. */
 
-        return thedata;
-}
+	struct pc_data consumer_receive(void)
+	{
+		struct pc_data thedata;
+			 
+		P(full);
+		P(mutex);
+			
+		thedata = buffer.elements[buffer.first];
+		buffer.first = (buffer.first + 1)%BUFFER_SIZE;
+			
+		V(mutex);
+		V(empty);
+		return thedata;		
+	}
 
-/* procucer_send() is called by a producer to store data in your
-   bounded buffer. */
 
-void producer_send(struct pc_data item)
-{
-        (void) item; /* Remove this when you add your code */
-}
+	/* procucer_send() is called by a producer to store data in your
+	   bounded buffer. */
+
+	void producer_send(struct pc_data item)
+	{
+		P(empty);
+		P(mutex);
+		
+		buffer.elements[buffer.last] = item;
+		buffer.last = (buffer.last + 1)%BUFFER_SIZE;
+		
+		V(mutex);
+		V(full);
+	}
 
 
 
 
-/* Perform any initialisation (e.g. of global data) you need
-   here. Note: You can panic if any allocation fails during setup */
+	/* Perform any initialisation (e.g. of global data) you need
+	   here. Note: You can panic if any allocation fails during setup */
 
-void producerconsumer_startup(void)
-{
-}
+	void producerconsumer_startup(void)
+	{
+		
+		mutex = sem_create("mutex", 1);
+		empty = sem_create("empty", BUFFER_SIZE);
+		full = sem_create("full", 0);
+		
+	}
 
-/* Perform any clean-up you need here */
-void producerconsumer_shutdown(void)
-{
-}
+	/* Perform any clean-up you need here */
+	void producerconsumer_shutdown(void)
+	{
+		sem_destroy(mutex);
+		sem_destroy(empty);
+		sem_destroy(full);
+	}
 
--- asst1-src/kern/compile/ASST1/.depend.producerconsumer.c
+++ asst1-src/kern/compile/ASST1/.depend.producerconsumer.c
@@ -1,3 +1,5 @@
 producerconsumer.o: ../../asst1/producerconsumer.c ../../include/types.h \
  ../../include/kern/types.h includelinks/kern/machine/types.h \
- includelinks/machine/types.h ../../asst1/producerconsumer_driver.h
+ includelinks/machine/types.h ../../asst1/producerconsumer_driver.h \
+ ../../include/synch.h ../../include/spinlock.h ../../include/cdefs.h \
+ ../../include/hangman.h opt-hangman.h includelinks/machine/spinlock.h
